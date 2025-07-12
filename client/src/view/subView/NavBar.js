2a192e669c270fac9e4c4a43e094de578082c423
Satvik Reddy Konda <0skonda31@gmail.com>
2025-07-12 16:14:28 +0900

    css updates

--- client/src/view/subView/NavBar.js
+++ client/src/view/subView/NavBar.js
@@ -15,7 +15,7 @@ export default function NavBar(props) {
   };
 
   return (
-    <nav style={{ padding: "1rem", background: "#708A58" }}>
+    <nav style={{ padding: "1rem", background: "#D5ED9F" }}>
       <Link to="/">Login</Link> |{" "}
       <Link to="/leaderboard">LeaderBoard</Link> |{" "}
       <Link to="/game">Game</Link>
